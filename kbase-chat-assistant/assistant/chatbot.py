ce9a24f9810f8655420df98f3c0691c6d865fcfe
Prachi Gupta <prachigta@gmail.com>
2024-04-29 17:57:55 +0000

    Remove kbase-chat-assistant directory

--- kbase-chat-assistant/assistant/chatbot.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.language_models.llms import LLM
-
-class KBaseChatBot:
-    _llm: LLM
-    def __init__(self: "KBaseChatBot",  llm: LLM) -> None:
-        self._llm = llm
-        
--- kbase-chat-assistant/embeddings/embeddings.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-DEFAULT_CATALOG_DB_DIR: Path = Path(__file__).parent / "vector_db_app_catalog"
-DEFAULT_DOCS_DB_DIR: Path = Path(__file__).parent / "vector_db_kbase_docs"
-
-HF_CATALOG_DB_DIR: Path = Path(__file__).parent / "HFvector_db_app_catalog"
-HF_DOCS_DB_DIR: Path = Path(__file__).parent / "HFvector_db_kbase_docs"
